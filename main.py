161b0d9250867c3b645a5e05c3b941ba1708d371
S.P. Mohanty <spmohanty91@gmail.com>
2018-03-05 23:33:12 +0100

    Aggregate polygons with transformed coordinates

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def process_tile(xml_path, image_path, geojson_path, segmentation_path):
         # continue
 
     g = json.loads(open(geojson_path).read())
+    _polygons = []
     for f in g["features"]:
         """
         TODO: Assert that this is a Polygon feature
@@ -36,14 +37,17 @@ def process_tile(xml_path, image_path, geojson_path, segmentation_path):
         if (f["type"] == "Feature") and "geometry" in f.keys():
             polygons = f["geometry"]["coordinates"]
             for polygon in polygons:
+                _polygon = []
                 for coord in polygon:
                     if type(coord) == list and len(coord) == 3:
                             X, Y = lat_long_to_pixel(tiff_source, coord[0], coord[1])
-                            print(X, Y, coord)
-
+                            _polygon.append([X, Y])
+                _polygons.append(_polygon)
+                print(_polygon)
         """
         TODO: Aggregate into the correct JSON structure
         """
+    print("Locally aggregated Polygons : ", _polygons)
 if __name__ == "__main__":
     ms_coco_format = process_tile(xml_path, image_path, geojson_path, segmentation_path)
     print(ms_coco_format)
