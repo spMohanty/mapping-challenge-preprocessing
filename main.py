ea6ed136d18fad1309b02dee1f3be9dd542755ba
S.P. Mohanty <spmohanty91@gmail.com>
2018-03-07 19:19:31 +0100

    Move process_tile function into helpers

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ from PIL import Image
 import numpy as np
 import os
 import shutil
-import xmltodict
 import json
-from osgeo import gdal
-from helpers import lat_long_to_pixel, rotate, compute_annotations
-import math
 
 import templates
+from helpers import process_tile
+
 
 import json
 """
@@ -26,68 +24,6 @@ image_path = "examples/image.jpg"
 geojson_path = "examples/buildings.geojson"
 segmentation_path = "examples/segmentation.png"
 
-
-def process_tile(image_id, xml_path, image_path, geojson_path, segmentation_path, rotation=0):
-    """
-    Processes a single tile and returns the corresponding object in
-    MS Coco format
-
-    :param:
-        rotation : Only support rotataion values of 0, 90, -90, 180
-    """
-    assert rotation in [0, math.pi/2, -1*math.pi/2, math.pi]
-    xml = xmltodict.parse(open(xml_path).read())
-    tiff_source = gdal.Open(xml["annotation"]["filename"])
-    tile_width = int(xml["annotation"]["size"]["width"])
-    tile_height = int(xml["annotation"]["size"]["height"])
-    number_of_buildings = len(xml["annotation"]["object"])
-    if number_of_buildings == 0:
-        "Ignore this"
-        return False
-
-    annotations = []
-
-    i = 0
-    g = json.loads(open(geojson_path).read())
-
-    for f in g["features"]:
-        _polygons = []
-        """
-        TODO: Assert that this is a Polygon feature
-        """
-        if (f["type"] == "Feature") and "geometry" in f.keys():
-            polygons = f["geometry"]["coordinates"]
-            for polygon in polygons:
-                _polygon = []
-
-                for coord in polygon:
-                    if type(coord) == list and len(coord) == 3:
-                        X, Y = lat_long_to_pixel(tiff_source, coord[0], coord[1])
-                        if rotation:
-                            X, Y = rotate((tile_width/2, tile_height/2), (X, Y), rotation )
-                        _polygon.extend([X, Y])
-
-                _polygons.append(_polygon)
-
-            segmentation, bbox, area = compute_annotations(
-                                            _polygons,
-                                            tile_width,
-                                            tile_height,
-                                            poly_format=True)
-
-            annotation = templates.annotataion(
-                                id=i+1,
-                                image_id=image_id,
-                                segmentation=segmentation,
-                                area=np.float(area),
-                                bbox=bbox,
-                                category_id=100,
-                                is_crowd=0
-                                )
-            annotations.append(annotation)
-
-        i = i + 1
-    return annotations
 
 
 
