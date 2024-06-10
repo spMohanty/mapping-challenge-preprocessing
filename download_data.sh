640481eb0e89672a2ebf6570f33744edca6fcfbb
Sharada Prasanna Mohanty <spmohanty91@gmail.com>
2024-06-10 20:25:17 +0200

    update s3 uris to original datasets

--- download_data.sh
+++ download_data.sh
@@ -12,24 +12,24 @@
 # https://spacenet.ai/spacenet-buildings-dataset-v2/
 
 # Shanghai
-mkdir -p data/AOI_4_Shanghai/
-aws s3api get-object --bucket spacenet-dataset --key AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz --request-payer requester data/AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz &
-aws s3api get-object --bucket spacenet-dataset --key AOI_4_Shanghai/AOI_4_Shanghai_Test_public.tar.gz --request-payer requester data/AOI_4_Shanghai/AOI_4_Shanghai_Test_public.tar.gz &
+# mkdir -p data/AOI_4_Shanghai/
+# aws s3api get-object --bucket spacenet-dataset --key AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz --request-payer requester data/AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz &
+# aws s3api get-object --bucket spacenet-dataset --key AOI_4_Shanghai/AOI_4_Shanghai_Test_public.tar.gz --request-payer requester data/AOI_4_Shanghai/AOI_4_Shanghai_Test_public.tar.gz &
 
-# Vegas
-mkdir -p data/AOI_2_Vegas/
-aws s3api get-object --bucket spacenet-dataset --key AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz --request-payer requester data/AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz &
-aws s3api get-object --bucket spacenet-dataset --key AOI_2_Vegas/AOI_2_Vegas_Test_public.tar.gz --request-payer requester data/AOI_2_Vegas/AOI_2_Vegas_Test_public.tar.gz &
+# # Vegas
+# mkdir -p data/AOI_2_Vegas/
+# aws s3api get-object --bucket spacenet-dataset --key AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz --request-payer requester data/AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz &
+# aws s3api get-object --bucket spacenet-dataset --key AOI_2_Vegas/AOI_2_Vegas_Test_public.tar.gz --request-payer requester data/AOI_2_Vegas/AOI_2_Vegas_Test_public.tar.gz &
 
-#Paris
-mkdir -p data/AOI_3_Paris/
-aws s3api get-object --bucket spacenet-dataset --key AOI_3_Paris/AOI_3_Paris_Train.tar.gz --request-payer requester data/AOI_3_Paris/AOI_3_Paris_Train.tar.gz & 
-aws s3api get-object --bucket spacenet-dataset --key AOI_3_Paris/AOI_3_Paris_Test_public.tar.gz --request-payer requester data/AOI_3_Paris/AOI_3_Paris_Test_public.tar.gz &
+# #Paris
+# mkdir -p data/AOI_3_Paris/
+# aws s3api get-object --bucket spacenet-dataset --key AOI_3_Paris/AOI_3_Paris_Train.tar.gz --request-payer requester data/AOI_3_Paris/AOI_3_Paris_Train.tar.gz & 
+# aws s3api get-object --bucket spacenet-dataset --key AOI_3_Paris/AOI_3_Paris_Test_public.tar.gz --request-payer requester data/AOI_3_Paris/AOI_3_Paris_Test_public.tar.gz &
 
-#Khartoum
-mkdir -p data/AOI_5_Khartoum/
-aws s3api get-object --bucket spacenet-dataset --key AOI_5_Khartoum/AOI_5_Khartoum_Train.tar.gz --request-payer requester data/AOI_5_Khartoum/AOI_5_Khartoum_Train.tar.gz &
-aws s3api get-object --bucket spacenet-dataset --key AOI_5_Khartoum/AOI_5_Khartoum_Test_public.tar.gz --request-payer requester data/AOI_5_Khartoum/AOI_5_Khartoum_Test_public.tar.gz &
+# #Khartoum
+# mkdir -p data/AOI_5_Khartoum/
+# aws s3api get-object --bucket spacenet-dataset --key AOI_5_Khartoum/AOI_5_Khartoum_Train.tar.gz --request-payer requester data/AOI_5_Khartoum/AOI_5_Khartoum_Train.tar.gz &
+# aws s3api get-object --bucket spacenet-dataset --key AOI_5_Khartoum/AOI_5_Khartoum_Test_public.tar.gz --request-payer requester data/AOI_5_Khartoum/AOI_5_Khartoum_Test_public.tar.gz &
 
 
 
@@ -42,6 +42,14 @@ aws s3api get-object --bucket spacenet-dataset --key AOI_5_Khartoum/AOI_5_Kharto
 # aws s3api get-object --bucket spacenet-dataset --key SpaceNet_Roads_Competition/AOI_4_Shanghai_Roads_Train.tar.gz --request-payer requester AOI_4_Shanghai_Roads_Train.tar.gz &
 # aws s3api get-object --bucket spacenet-dataset --key SpaceNet_Roads_Competition/AOI_5_Khartoum_Roads_Test_Public.tar.gz --request-payer requester AOI_5_Khartoum_Roads_Test_Public.tar.gz &
 # aws s3api get-object --bucket spacenet-dataset --key SpaceNet_Roads_Competition/AOI_5_Khartoum_Roads_Train.tar.gz --request-payer requester AOI_5_Khartoum_Roads_Train.tar.gz &
+
+
+
+mkdir data/
+aws s3 cp s3://spacenet-dataset/spacenet/SN2_buildings/train/tarballs/SN2_buildings_train_AOI_2_Vegas.tar.gz data/SN2_buildings_train_AOI_2_Vegas.tar.gz & 
+aws s3 cp s3://spacenet-dataset/spacenet/SN2_buildings/train/tarballs/SN2_buildings_train_AOI_3_Paris.tar.gz data/SN2_buildings_train_AOI_3_Paris.tar.gz & 
+aws s3 cp s3://spacenet-dataset/spacenet/SN2_buildings/train/tarballs/SN2_buildings_train_AOI_4_Shanghai.tar.gz data/SN2_buildings_train_AOI_4_Shanghai.tar.gz & 
+aws s3 cp s3://spacenet-dataset/spacenet/SN2_buildings/train/tarballs/SN2_buildings_train_AOI_5_Khartoum.tar.gz data/SN2_buildings_train_AOI_5_Khartoum.tar.gz &
 
 
 
