3e4a1d0f4eaf13cbb6de99c704c57b89c1d515fd
Sharada Prasanna Mohanty <spmohanty91@gmail.com>
2024-06-20 05:19:49 +0200

    Update prepare-data scripts

--- README.md
+++ README.md
@@ -3,11 +3,33 @@
 
 **TODO**: Add introduction, installation, usage
 
+# Install 
+```
+# This is a fork of the spacenetv2 utilities repo, which adds the py3 support + parallelization using ray
+git clone git@github.com:spMohanty/spacenetv2-utilities-py3.git utilities
+cd utilities 
+pip install -e .
+
+cd ../
+git clone git@github.com:spMohanty/mapping-challenge-preprocessing.git
+cd mapping-challenge-preprocessing
+pip install -r requirements.txt
+```
+
 # Usage
 ```
-./download_data.sh
-./arrange.sh
-python main.py
+./download_data.sh # please refer to the inline comments to untar the files
+
+# generte the RGB data and the PASCALVOC annotations
+export DATASET_DIRECTORY="/scratch/mohanty/mapping-challenge-data" # update this path to your local path
+
+DATASET_NAME="AOI_2_Vegas_Train" ./prepare-data.sh
+DATASET_NAME="AOI_3_Paris_Train" ./prepare-data.sh
+DATASET_NAME="AOI_4_Shanghai_Train" ./prepare-data.sh
+DATASET_NAME="AOI_5_Khartoum_Train" ./prepare-data.sh
+
+
+# python main.py
 ```
 
 # Authors
---
+++ prepare-data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+
+# Check if DATASET_DIRECTORY is already set, if not set it to the default value
+if [ -z "$DATASET_DIRECTORY" ]; then
+  export DATASET_DIRECTORY="/scratch/mohanty/mapping-challenge-data"
+fi
+
+# Check if DATASET_NAME is already set, if not set it to the default value
+if [ -z "$DATASET_NAME" ]; then
+  export DATASET_NAME="AOI_2_Vegas_Train"
+fi
+
+export ANNOTATIONS_DIRECTORY="$DATASET_DIRECTORY/$DATASET_NAME/PASCALVOC_annotations"
+
+if [ -d $ANNOTATIONS_DIRECTORY ]; then
+  echo "Annotations Directory exists already. Deleting it before proceeding: $ANNOTATIONS_DIRECTORY."
+  rm -rf $ANNOTATIONS_DIRECTORY
+fi
+
+export PYTHONPATH="$PYTHONPATH:../utilities/python"
+
+python ../utilities/python/createDataSpaceNet.py $DATASET_DIRECTORY/$DATASET_NAME/ \
+           --srcImageryDirectory RGB-PanSharpen \
+           --outputDirectory $ANNOTATIONS_DIRECTORY \
+           --annotationType PASCALVOC2012 \
+           --outputFileType JPEG \
+           --convertTo8Bit \
+           --imgSizePix 300 \
+           --trainTestSplit 0.8
